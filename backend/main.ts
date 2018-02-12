22d64eb06376e5a9001e5064a598cd652ac17c31
kml183 <kml183@mail.usask.ca>
2018-02-11 18:53:46 -0600

    update backend to use buffer instead of atob module which doesn't have an interface

--- backend/main.ts
+++ backend/main.ts
@@ -1,3 +1,5 @@
+import { json } from "body-parser";
+
 const Datastore = require('@google-cloud/datastore');
 const datastore = Datastore();
 const kind = 'RoomDataPoint';
@@ -7,31 +9,31 @@ export interface RoomDataPoint {
     humidity: number,
     ambientLight: number,
     timestamp: string
+    cpuTemp: string
 }
 
-export interface RaspberryPiRoomMonitoringPubSubMessage {
-    timestamp: string,
-    data: {  
-       attributes: {  
-          temperature: number,
-          humidity: number,
-          ambientLight: number
-       }
+// Structure of the pubsub message for needed fields 
+export interface RaspberryPiRoomMonitoringPubSubMessage {  
+    data:{  
+       data: string; // base 64 encoded string
     }
-}
+ }
 
 export function RaspberryPiRoomMonitoringPubSubHandler(req: RaspberryPiRoomMonitoringPubSubMessage, callback: Function) {
-    console.log(req);
-    let timestamp = new Date(req.timestamp);
+    let dataString = (Buffer.from(req.data.data, 'base64').toString());
+    let data = JSON.parse(dataString) as RoomDataPoint;
+    if(!data) return;
 
+    let timestamp = new Date(data.timestamp);
     let dateKey = timestamp.getTime().toString()
+
     const key = datastore.key([kind, dateKey])
     const entity = {
         key: key,
         data: {
-          temperature: req.data && req.data.attributes ? req.data.attributes.temperature : null,
-          humidity: req.data && req.data.attributes ? req.data.attributes.humidity : null,
-          ambientLight: req.data && req.data.attributes ? req.data.attributes.ambientLight : null,
+          temperature: data ? data.temperature : null,
+          humidity: data ? data.humidity : null,
+          ambientLight: data ? data.ambientLight : null,
           timestamp: timestamp.toString()
         } as RoomDataPoint
     };
