5abb2fdc5ac58db7d062d64a896aed57e0c16e7c
kml183 <kml183@mail.usask.ca>
2018-03-24 23:44:45 -0600

    update server code

--- server/main.py
+++ server/main.py
@@ -1,58 +1,50 @@
 from flask import Flask
 from flask_restful import Resource, Api, fields, marshal_with
-import Adafruit_CCS811
+from Adafruit_CCS811 import Adafruit_CCS811
 import subprocess
 from time import gmtime, strftime
 import json
 
 app = Flask(__name__)
 api = Api(app)
-css811_data = Adafruit_CCS811()
+css811_client = Adafruit_CCS811()
 
-data_fields = {
-    'temperature':   fields.Float,
+ROOM_ENVIRONMENT_DATA_FIELDS = {
+    'temp':   fields.Float,
     'humidty':    fields.Float,
-    'ambientLight': fields.Float,
+    'ambient_light': fields.Float,
     'timestamp': fields.String,
-    'cpuTemp': fields.Float,
+    'cpu_temp': fields.Float,
     'tvoc': fields.Float,
     'co2': fields.Float,
 }
 
-
 class GetRoomEnvironmentData(Resource):
-    @marshal_with(data_fields)
+    @marshal_with(ROOM_ENVIRONMENT_DATA_FIELDS)
     def get(self):
-        data = {
-            "temperature": 25,
-            "humidity": 334,
-            "ambientLight": 345,
-            "timestamp": "time",
-            "cpuTemp": 43,
-            "tvoc": 0,
-            "co2": 0,
-        }
-        return data
+        return self.collect_data()
 
     def collect_data(self):
 
+        ccs811_data = self.ccs811()
+
         # Get the Raspberry Pi's processor temperature. 
         if subprocess.call(["which", "/opt/vc/bin/vcgencmd"]) == 0:
-            temp = subprocess.check_output(["sudo", "/opt/vc/bin/vcgencmd", "measure_temp"]).split('=', 1)[-1].rstrip()
+            cpu_temp = subprocess.check_output(["sudo", "/opt/vc/bin/vcgencmd", "measure_temp"]).split('=', 1)[-1].rstrip()
         # Get Mac's proc temp if "sudo gem install iStats" is installed 
         elif subprocess.call(["which", "istats"]) == 0:
             #temp = subprocess.check_output(["istats"]).decode().split(":")[-1].split("\n")[0]
-            temp = "".join(subprocess.check_output(["istats"]).decode().split()).split(":")[-1].split("F")[0]
+            cpu_temp = "".join(subprocess.check_output(["istats"]).decode().split()).split(":")[-1].split("F")[0]
         else:
-            temp = 40
+            cpu_temp = 40
 
         result = {
-            'temperature': 0,
-            "ambientLight": 4000,
+            "ambient_light": 4000,
             "humidity": 3000,
-            "cpuTemp": css811_data.get("temp"),
-            "tvoc": css811_data.get("tvoc"),
-            "co2": css811_data.get("co2"),
+            "temp": ccs811_data.get("temp"),
+            "cpu_temp": cpu_temp,
+            "tvoc": ccs811_data.get("tvoc"),
+            "co2": ccs811_data.get("co2"),
             "timestamp": strftime("%Y-%m-%d %H:%M:%S", gmtime())
         }
 
@@ -60,20 +52,20 @@ class GetRoomEnvironmentData(Resource):
 
     def ccs811(self):
 
-        if css811_data.available():
-            temp = css811_data.calculateTemperature()
-            if not css811_data.readData():
+        if css811_client.available():
+            temp = css811_client.calculateTemperature()
+            if not css811_client.readData():
                 return {
-                    "co2": css811_data.geteCO2(),
-                    "tvoc": css811_data.getTVOC(),
+                    "co2": css811_client.geteCO2(),
+                    "tvoc": css811_client.getTVOC(),
                     "temp": temp
                 }
 
 def init_sensors():
-    while not css811_data.available():
+    while not css811_client.available():
         pass
-    temp = css811_data.calculateTemperature()
-    css811_data.tempOffset = temp - 25.0
+    temp = css811_client.calculateTemperature()
+    css811_client.tempOffset = temp - 25.0
 
 def main():
     # Setup flask app
