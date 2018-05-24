63b4c60166d3208150ea5c9bad6f4bc1520010c6
Kevin Lutzer <kml183@mail.usask.ca>
2018-05-23 22:21:33 -0600

    stub out most of the project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RoomMonitorDataService } from './room-monitor-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  constructor( roomMonitorService: RoomMonitorDataService) {}
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MetricBoxComponent } from './metric-box.component';
+import { RoomMonitorDataService } from './room-monitor-data.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MetricBoxComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [RoomMonitorDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
