8d575510ffe47cfe3e0f0ce4249156e8961a4db2
Kevin Lutzer <kml183@mail.usask.ca>
2018-05-25 19:55:20 -0600

    make more progress on metric boxes

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule, MatCardHeader } from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 import { AppComponent } from './app.component';
 import { MetricBoxComponent } from './metric-box.component';
 import { RoomMonitorDataService } from './room-monitor-data.service';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +14,9 @@ import { RoomMonitorDataService } from './room-monitor-data.service';
     MetricBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [RoomMonitorDataService],
   bootstrap: [AppComponent]
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
-# from Adafruit_CCS811 import Adafruit_CCS811
-# from tsl2561 import TSL2561
-# import subprocess
-# from time import gmtime, strftime
-# import json
-# import time
+from Adafruit_CCS811 import Adafruit_CCS811
+from tsl2561 import TSL2561
+import subprocess
+from time import gmtime, strftime
+import json
+import time
 import argparse
 
 def parse_command_line_args():
@@ -20,78 +20,69 @@ def parse_command_line_args():
         help="The sensor to get data for")
     return parser.parse_args()
 
+# Returns serialized data from the tsl2561 sensor
+def tsl2561():
+    tsl1261_client = TSL2561(debug=False)
+    data = tsl1261_client.lux()
+    return json.dumps({
+        "lux": data
+    })
 
-# # # returns the serialized data for the cpu temp
-# def cpu_temp():
-#     data = subprocess.check_output(["sudo", "/opt/vc/bin/vcgencmd", "measure_temp"]).split('=', 1)[-1].rstrip()
-#     return json.dumps({
-#         "cpu_temp": data
-#     })
+# Returns serialized data from the ccs811 sensor
+def ccs811():
+    ccs811_client = Adafruit_CCS811()
 
-# # # Returns serialized data from the tsl2561 sensor
-# def tsl2561():
-#     tsl1261_client = TSL2561(debug=False)
-#     data = tsl1261_client.lux()
-#     return json.dumps({
-#         "lux": data
-#     })
-
-# # # Returns serialized data from the ccs811 sensor
-# def ccs811():
-#     ccs811_client = Adafruit_CCS811()
-
-#     data = None
-#     if not ccs811_client.readData():
-#         data = {
-#             "co2": ccs811_client.geteCO2(),
-#             "tvoc": ccs811_client.getTVOC(),
-#             "temp": ccs811_client.calculateTemperature()
-#         }
-#     else:
-#         raise RuntimeError('Was not able to read air quality from the ccs811.')
-
-#     return json.dumps(data)
-
-# def init_ccs811():
-#     ccs811_client = Adafruit_CCS811()
-
-#     while not ccs811_client.available():
-#         pass
-    
-#     # Calibrate temperature
-#     temp = ccs811_client.calculateTemperature()
-#     ccs811_client.tempOffset = temp - 25.0
-
-#     print "success"
-
-def evaluateStub(sensor):
-    if sensor == "light":
-        print "{\"lux\": 4000}"
-    elif sensor == "initialize_light":
-        print "success"
-    elif sensor == "gas":
-        print "{\"temp\": 43.2, \"tvoc\": 4000, \"co2\": 4000.5}"
+    data = None
+    if not ccs811_client.readData():
+        data = {
+            "co2": ccs811_client.geteCO2(),
+            "tvoc": ccs811_client.getTVOC(),
+            "temp": ccs811_client.calculateTemperature()
+        }
     else:
-        raise RuntimeError("Not a valid sensor")
+        raise RuntimeError('Was not able to read air quality from the ccs811.')
 
-# def evaluateSensors(sensor):
+    return json.dumps(data)
+
+def init_ccs811():
+    ccs811_client = Adafruit_CCS811()
+
+    while not ccs811_client.available():
+        pass
+    
+    # Calibrate temperature
+    temp = ccs811_client.calculateTemperature()
+    ccs811_client.tempOffset = temp - 25.0
+
+    print "success"
+
+# def evaluateStub(sensor):
 #     if sensor == "light":
-#         print tsl2561()
+#         print "{\"lux\": 4000}"
 #     elif sensor == "initialize_light":
-#         init_ccs811()
 #         print "success"
 #     elif sensor == "gas":
-#         print ccs811()
-#     else: 
+#         print "{\"temp\": 43.2, \"tvoc\": 4000, \"co2\": 4000.5}"
+#     else:
 #         raise RuntimeError("Not a valid sensor")
+
+def evaluateSensors(sensor):
+    if sensor == "light":
+        print tsl2561()
+    elif sensor == "initialize_light":
+        init_ccs811()
+        print "success"
+    elif sensor == "gas":
+        print ccs811()
+    else: 
+        raise RuntimeError("Not a valid sensor")
 
 def main():
 
     args = parse_command_line_args()
 
     val = args.sensor
-    evaluateStub(val)
+    evaluateSensors(val)
     
-        
 if __name__ == '__main__':
     main()
