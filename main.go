79258828ed37c599e6e9595e9a7ae7c4d873a3cb
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-09-13 12:48:32 -0600

    add some base code I will need to in the entry point

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"log"
 	"os"
 	"runtime"
+
+	"gobot.io/x/gobot/drivers/i2c"
 
 	s "github.com/kml183/room-environment-monitor/internal/server"
 )
@@ -27,4 +31,27 @@ func main() {
 	if err := s.NewHTTPServer(logger, stub); err != nil {
 		logger.Fatalf("Could not start the http server > %s \n", err.Error())
 	}
+}
+
+func adafruitCCS811BreakoutBoard(a *i2c.CCS811Driver) {
+	v, err := a.GetHardwareVersion()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	fmt.Printf("Hardare Version %#x\n", v)
+
+	d, err := a.GetFirmwareBootVersion()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	fmt.Printf("Boot Version %#x\n", d)
+
+	d, err = a.GetFirmwareAppVersion()
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	fmt.Printf("App Version %#x\n", d)
 }
