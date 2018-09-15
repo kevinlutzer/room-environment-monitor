384c39b0ddcf729c7dbd9664033351f6f75a23a6
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-09-15 14:51:39 -0600

    update build deploy script so that it cleans up files and actually works

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,3 +1,4 @@
 GOARM=6 GOARCH=arm GOOS=linux go build -o main main.go # Build for raspi architecture
 scp main pi@192.168.1.111:/home/pi/
-ssh -t pi@192.168.1.111 "./home/pi/main"
+rm main
+ssh -t pi@192.168.1.111 "cd ~/ && ./main"
