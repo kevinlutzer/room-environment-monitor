0a167b34e473bcfde5efd7493a1ee3a6bc4218a5
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-09-25 01:20:44 -0600

    successfully move over experimental iot api stuff over to new structure

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,28 +3,23 @@ import * as admin   from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
 
-import { RoomEnvironmentMonitorHandler } from './api/room_environment_monitor_handler';
-import { IOTFullfillmentHandler } from './api/iot_fullfillment';
+import { RoomEnvironmentMonitorHandler } from './controllers/room_environment_monitor_handler';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({timestampsInSnapshots: true});
 
 const roomEnvironmentHandler = new RoomEnvironmentMonitorHandler();
-const iotFullfillmentHandler = new IOTFullfillmentHandler();
 
 const roomEnvironmentMonitorApp = express();
 roomEnvironmentMonitorApp.use(cors({ origin: true }));
 
-const iotFullFillmentApp = express();
-iotFullFillmentApp.use(cors({ origin: true }));
-
 // CRUD interfaces:
 roomEnvironmentMonitorApp.get('/list', (req, res) => roomEnvironmentHandler.List(req, res, db));
-iotFullFillmentApp.post('/', iotFullfillmentHandler.FullFillment);
 
 // Pubsub handlers
 exports.RoomEnvironmentMonitorPubsubHandler = functions.pubsub.topic('room-monitor-telemetry').onPublish((message) => roomEnvironmentHandler.Pubsub(message, db))
 exports.RoomEnvironmentMonitorHandler = functions.https.onRequest(roomEnvironmentMonitorApp);
 
-exports.IOTFullfillmentHandler = functions.https.onRequest(iotFullFillmentApp)
+
+export {IOTFullfillmentHandler} from './api/iot_fullfillment';
