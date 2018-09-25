0ef5c100843a1e35d2158c061e92db2211aa5649
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-09-25 00:44:55 -0600

    get a base function to work

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,20 +2,29 @@ import * as functions from 'firebase-functions';
 import * as admin   from 'firebase-admin';
 import * as express from 'express';
 import * as cors from 'cors';
+
 import { RoomEnvironmentMonitorHandler } from './api/room_environment_monitor_handler';
+import { IOTFullfillmentHandler } from './api/iot_fullfillment';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({timestampsInSnapshots: true});
 
-const handler = new RoomEnvironmentMonitorHandler();
+const roomEnvironmentHandler = new RoomEnvironmentMonitorHandler();
+const iotFullfillmentHandler = new IOTFullfillmentHandler();
 
-const app = express();
-app.use(cors({ origin: true }));
+const roomEnvironmentMonitorApp = express();
+roomEnvironmentMonitorApp.use(cors({ origin: true }));
+
+const iotFullFillmentApp = express();
+iotFullFillmentApp.use(cors({ origin: true }));
 
 // CRUD interfaces:
-app.get('/list', (req, res) => handler.List(req, res, db));
+roomEnvironmentMonitorApp.get('/list', (req, res) => roomEnvironmentHandler.List(req, res, db));
+iotFullFillmentApp.post('/', iotFullfillmentHandler.FullFillment);
 
 // Pubsub handlers
-exports.RoomEnvironmentMonitorPubsubHandler = functions.pubsub.topic('room-monitor-telemetry').onPublish((message) => handler.Pubsub(message, db))
-exports.RoomEnvironmentMonitorHandler = functions.https.onRequest(app);
+exports.RoomEnvironmentMonitorPubsubHandler = functions.pubsub.topic('room-monitor-telemetry').onPublish((message) => roomEnvironmentHandler.Pubsub(message, db))
+exports.RoomEnvironmentMonitorHandler = functions.https.onRequest(roomEnvironmentMonitorApp);
+
+exports.IOTFullfillmentHandler = functions.https.onRequest(iotFullFillmentApp)
