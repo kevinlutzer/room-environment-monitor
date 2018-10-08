08ef7330588d4fdaa2adb992f13377c8baf9ba5c
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-10-08 16:30:27 -0600

    properly exit

--- build-deploy.sh
+++ build-deploy.sh
@@ -3,7 +3,9 @@ if [ $# -eq 0 ]
   then
     echo "IP address needs to be supplied as arugment 1"
 fi
+exit 0
 
 GOARM=5 GOARCH=arm GOOS=linux go build -o main main.go # Build for raspi architecture specifically the raspberry pi w arm processors
 scp main pi@$1:/home/pi/
+rm main
 ssh -t pi@$1 "cd ~/ && ./main"
