d3d28e644ec8d8a2a32cb6aedae40c30a476fdcf
Kevin Lutzer <kevinlutzer9@gmail.com>
2018-10-10 00:27:10 -0600

    refactor to remove cloud actions and fullfillment stuff

--- functions/src/controllers/room_environment_monitor.ts
+++ functions/src/controllers/room_environment_monitor.ts
@@ -12,39 +12,45 @@ db.settings({timestampsInSnapshots: true});
     // Handlers
 export const roomEnvironmentMonitorPubsubHandler = async (message: functions.pubsub.Message) => {
 
-    console.log(message)
-    let rawData: RoomEnvironmentMonitorPubsubMessageInterface;
+    console.log("message: ", message)
+
+    // Wrap whole thing in try catch. If there is an error for some pubsub message, the message will not be acked off the pubsub queue
     try {
-        rawData = JSON.parse(message.data ? Buffer.from(message.data, 'base64').toString() : null);
-    } catch {
-        console.error("Failed to parse the pubsub mesage. The api interface most likely changed");
+        let rawData: RoomEnvironmentMonitorPubsubMessageInterface;
+        try {
+            rawData = JSON.parse(message.data ? Buffer.from(message.data, 'base64').toString() : null);
+        } catch {
+            console.error("Failed to parse the pubsub mesage. The api interface most likely changed");
+            return null;
+        }
+
+        // Becasue the id is based on the timestamp, if the timestamp is passed null or undefined the same entity will be updated
+        const sysDate = new Date(rawData.timestamp || "");
+        const id = sysDate.getTime().toString();
+
+        const data = {
+            lux: rawData.lux || 0,
+            co2: rawData.co2 || 0,
+            tvoc: rawData.tvoc || 0,
+            roomTemp: rawData.room_temp || 0,
+            cpuTemp: rawData.cpu_temp || 0,
+            pressure: rawData.pressure || 0, 
+            humidity: rawData.humidity || 0,
+            timestamp: admin.firestore.Timestamp.fromDate(sysDate),
+        } as RoomEnvironmentMonitorTelemetry
+
+        return db
+        .collection('RoomMonitorTeletry')
+        .doc(id)
+        .set(data)
+        .then(
+            resp => console.log("Added new entity with id: ", id, " and resp: ", resp),
+            err => console.error("Failed to create entity: ", err)
+        );
+    } catch (err) {
+        console.error(err);
         return null;
     }
-    console.log("message", message, "rawData", rawData);
-
-    // Becasue the id is based on the timestamp, if the timestamp is passed null or undefined the same entity will be updated
-    const sysDate = new Date(rawData.timestamp || "");
-    const id = sysDate.getTime().toString();
-
-    const data = {
-        lux: rawData.lux || 0,
-        co2: rawData.co2 || 0,
-        tvoc: rawData.tvoc || 0,
-        roomTemp: rawData.room_temp || 0,
-        cpuTemp: rawData.cpu_temp || 0,
-        pressure: rawData.pressure || 0, 
-        humidity: rawData.humidity || 0,
-        timestamp: admin.firestore.Timestamp.fromDate(sysDate),
-    } as RoomEnvironmentMonitorTelemetry
-
-    return db
-    .collection('RoomMonitorTeletry')
-    .doc(id)
-    .set(data)
-    .then(
-        resp => console.log("Added new entity with id: ", id, " and resp: ", resp),
-        err => console.error("Failed to create entity: ", err)
-    );
 }
 
 export const roomEnvironmentDataList = async(req: Request, res: Response) => {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,15 +1,2 @@
+// Cloud functions
 export * from './api/room_environment_monitor';
-export * from './api/actions_smart_home';
-
-import * as glob from "glob";
-const camelCase = require("camel-case");
-
-// The following block of code was grabbed from this tutorial https://codeburst.io/organizing-your-firebase-cloud-functions-67dc17b3b0da
-const files = glob.sync('./**/*.f.js', { cwd: __dirname, ignore: './node_modules/**'});
-for(let f=0,fl=files.length; f<fl; f++){
-  const file = files[f];
-  const functionName = camelCase(file.slice(0, -5).split('/').join('_')); // Strip off '.f.js'
-  if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
-    exports[functionName] = require(file);
-  }
-}
