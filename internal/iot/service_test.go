003a2394c84d16da6782280d141b634bf098a901
Kevin Lutzer <kml183@mail.usask.ca>
2019-01-29 22:07:56 -0600

    add base service test

---
+++ internal/iot/service_test.go
@@ -0,0 +1,14 @@
+package iot
+
+import (
+	"testing"
+)
+
+func Test_PublishSensorDataSnapshotHandler(t *testing.T) {
+	// loggingMock := &mocks.LoggerService{}
+	// sensorServiceMock := &mocks.SensorsService{}
+	// googleIOTServiceMock := &mocks.GoogleIOTService{}
+
+	// testService := NewIOTService(loggingMock, sensorServiceMock, googleIOTServiceMock)
+	// loggingMock.On()
+}
