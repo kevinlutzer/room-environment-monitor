127af16988d8d2f8e727689608c4eaee37d43f97
Kevin Lutzer <kml183@mail.usask.ca>
2019-01-30 18:58:14 -0600

    bring in travis ci

--- internal/iot/service_test.go
+++ internal/iot/service_test.go
@@ -2,8 +2,6 @@ package iot
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func Test_PublishSensorDataSnapshotHandler(t *testing.T) {
@@ -14,17 +12,22 @@ func Test_PublishSensorDataSnapshotHandler(t *testing.T) {
 	// testService := NewIOTService(loggingMock, sensorServiceMock, googleIOTServiceMock)
 	// loggingMock.On()
 
-	cases := []struct {
-		name           string
-		inputDrafts    string
-		inputPublished string
-		output         []string
-	}{}
+	// cases := []struct {
+	// 	name           string
+	// 	inputDrafts    string
+	// 	inputPublished string
+	// 	output         []string
+	// }{}
 
-	for _, c := range cases {
-		t.Run(c.name, func(t *testing.T) {
-			actual := ""
-			assert.Equal(t, c.output, actual)
-		})
-	}
+	// for _, c := range cases {
+	// 	t.Run(c.name, func(t *testing.T) {
+	// 		loggingMock := &mocks.LoggerService{}
+	// 		sensorServiceMock := &mocks.SensorsService{}
+	// 		googleIOTServiceMock := &mocks.GoogleIOTService{}
+
+	// 		testService := NewIOTService(loggingMock, sensorServiceMock, googleIOTServiceMock)
+	// 		testService.IntializeIOTFunctions()
+	// 		assert.Equal(t, c.output, actual)
+	// 	})
+	// }
 }
