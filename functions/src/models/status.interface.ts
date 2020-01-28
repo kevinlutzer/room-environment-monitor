d86aa2e5484f9d205fa396dcf4be7582278d127b
Kevin Lutzer <klutzer@vendasta.com>
2020-01-27 19:02:22 -0600

    move everything around

---
+++ functions/src/models/status.interface.ts
@@ -0,0 +1,31 @@
+import * as admin from 'firebase-admin';
+
+export const MODEL = "RoomEnvironmentMonitorStatus";
+
+export interface RoomEnvironmentMonitorStatusPubsubMessageInterface {
+    cpu_temp: number;
+    timestamp: string;
+}
+
+export interface RoomEnvironmentMonitorStatusInterface {
+    cpuTemp: number;
+    timestamp: admin.firestore.Timestamp;
+    deviceId: string;
+}
+
+export function Convert(deviceId: string, sysDate: Date, data: RoomEnvironmentMonitorStatusPubsubMessageInterface): RoomEnvironmentMonitorStatusInterface {
+    return {
+        cpuTemp: data.cpu_temp || 0,
+        timestamp: admin.firestore.Timestamp.fromDate(sysDate),
+        deviceId: deviceId,
+    } as RoomEnvironmentMonitorStatusInterface;
+}
+
+export function ConvertQuerySnapshotDocument(snapshot: FirebaseFirestore.QueryDocumentSnapshot): RoomEnvironmentMonitorStatusInterface {
+    const data = snapshot.data();
+    return {
+        cpuTemp: data.cpuTemp,
+        timestamp: data.timestamp ? data.timestamp.toDate() : new Date(),
+        deviceId: data.deviceId,
+    }
+}
--- functions/src/room_environment_monitor_config_webhook.ts
+++
@@ -1,7 +0,0 @@
-
-import { Response, Request, NextFunction } from 'express';
-
-// Handlers
-export async function SetConfig(req: Request, res: Response, next: NextFunction, db: FirebaseFirestore.Firestore) {
-    console.log(req);
-}
