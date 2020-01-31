5bb27a1d1ad1f84a8f07b021b04530d07d1d4b9c
Kevin Lutzer <klutzer@vendasta.com>
2020-01-31 13:49:42 -0500

    try and add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const cors = require('cors');
 
 import { TelemetryCreatePubsubHandler, TelemetryList,
     StatusUpdatePubsubHandler, StatusList, TelemetryGetLatest} from './controllers';
@@ -14,6 +15,25 @@ const db = admin.firestore();
 db.settings({timestampsInSnapshots: true});
 
 const RoomEnvironmentMonitor = express();
+
+var allowedOrigins = [
+    'https://website-klutzer.firebaseapp.com/overview',
+    'https://kevin.lutzer.ca/'
+];
+
+RoomEnvironmentMonitor.use(cors({
+    origin: function(origin, callback){
+      // allow requests with no origin 
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
 
 RoomEnvironmentMonitor.get('/api/telemetry/get', (req, res, next) => TelemetryGetLatest(req, res, next, db))
 RoomEnvironmentMonitor.get('/api/telemetry/list', (req, res, next) => TelemetryList(req, res, next, db))
