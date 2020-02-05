53890113ca1a5fb1301b06c2e4b94ddf06c072d6
Kevin Lutzer <klutzer@vendasta.com>
2020-02-04 23:39:05 -0500

    delete some stuff

--- functions/src/api/api-conversion.ts
+++ functions/src/api/api-conversion.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import * as functions from 'firebase-functions';
 
 export class ApiError {
     error: string;
@@ -62,4 +63,16 @@ export class ApiListResponse<T> {
             nextCursor: this.nextCursor,
         })
     }
+}
+
+export function ExtractInterfaceFromPubsubMessage(message: functions.pubsub.Message): any {
+    let rawData: any;
+    try {
+        rawData = JSON.parse(message.data ? Buffer.from(message.data, 'base64').toString() : null);
+    } catch {
+        console.error("Failed to parse the pubsub mesage. The api interface most likely changed");
+        return null;
+    }
+    console.log("message: ", message, ", rawData: ", rawData);
+    return rawData;
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,44 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
 
-import { TelemetryCreatePubsubHandler, TelemetryList,
-    StatusUpdatePubsubHandler, StatusList, TelemetryGetLatest} from './controllers';
-
-export const RoomEnvironmentTelemetryPubsubTopic = 'room-environment-monitor-telemetry';
-export const RoomEnvironmentStatusPubsubTopic = 'room-environment-monitor-status';
+import { StatusUpdatePubsubHandler } from './api';
 
 admin.initializeApp(functions.config().firebase);
 
 const db = admin.firestore();
 db.settings({timestampsInSnapshots: true});
 
-const RoomEnvironmentMonitor = express();
+// const RoomEnvironmentMonitor = express();
 
-var allowedOrigins = [
-    'https://website-klutzer.firebaseapp.com/overview',
-    'https://kevin.lutzer.ca/'
-];
+// RoomEnvironmentMonitor.get('/api/telemetry/get', (req, res, next) => TelemetryGetLatest(req, res, next, db))
+// RoomEnvironmentMonitor.get('/api/telemetry/list', (req, res, next) => TelemetryList(req, res, next, db))
 
-RoomEnvironmentMonitor.use(cors({
-    origin: function(origin, callback){
-      // allow requests with no origin 
-      // (like mobile apps or curl requests)
-      if(!origin) return callback(null, true);
-      if(allowedOrigins.indexOf(origin) === -1){
-        var msg = 'The CORS policy for this site does not ' +
-                  'allow access from the specified Origin.';
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    }
-  }));
-
-RoomEnvironmentMonitor.get('/api/telemetry/get', (req, res, next) => TelemetryGetLatest(req, res, next, db))
-RoomEnvironmentMonitor.get('/api/telemetry/list', (req, res, next) => TelemetryList(req, res, next, db))
-RoomEnvironmentMonitor.get('/api/status/list', (req, res, next) => StatusList(req, res, next, db))
-
-exports.TelemetryCreatePubsubHandler = functions.pubsub.topic(RoomEnvironmentTelemetryPubsubTopic).onPublish(msg => TelemetryCreatePubsubHandler(msg, db))
-exports.StatusUpsertPubsubHandler = functions.pubsub.topic(RoomEnvironmentTelemetryPubsubTopic).onPublish(msg => StatusUpdatePubsubHandler(msg, db))
-exports.RoomEnvironmentMonitor = functions.https.onRequest(RoomEnvironmentMonitor);
+export const TelemetryEventHandler = functions.pubsub.topic('room-environment-monitor-telemetry').onPublish(msg => StatusUpdatePubsubHandler(msg, db))
+// exports.RoomEnvironmentMonitor = functions.https.onRequest(RoomEnvironmentMonitor);
