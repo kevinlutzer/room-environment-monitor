5507e98d96ed5603a4818fe3e8d044cd6d4f6a95
Kevin Lutzer <klutzer@vendasta.com>
2020-02-05 21:55:06 -0500

    add cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,22 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const express = require('express');
+const cors = require('cors');
+
+import { config as functionsConfig, pubsub, https} from 'firebase-functions';
+import { initializeApp, firestore } from 'firebase-admin';
 
 import { TelemetryPubsubHandler, TelemetryEventList, DeviceList } from './api';
 
-admin.initializeApp(functions.config().firebase);
+initializeApp(functionsConfig().firebase);
 
-const db = admin.firestore();
+const db = firestore();
 db.settings({timestampsInSnapshots: true});
 
 const RoomEnvironmentMonitor = express();
 
+RoomEnvironmentMonitor.use(cors());
+
 RoomEnvironmentMonitor.get('/api/telemetry-event/list', (req, res, _) => TelemetryEventList(req, res, db))
 RoomEnvironmentMonitor.get('/api/device/list', (req, res, _) => DeviceList(req, res, db))
 
-
-exports.TelemetryEventPubsubHandler = functions.pubsub.topic('room-environment-monitor-telemetry').onPublish(msg => TelemetryPubsubHandler(msg, db))
-exports.RoomEnvironmentMonitor = functions.https.onRequest(RoomEnvironmentMonitor);
+exports.TelemetryEventPubsubHandler = pubsub.topic('room-environment-monitor-telemetry').onPublish(msg => TelemetryPubsubHandler(msg, db))
+exports.RoomEnvironmentMonitor = https.onRequest(RoomEnvironmentMonitor);
