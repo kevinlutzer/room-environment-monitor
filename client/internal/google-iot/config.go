e56fdf0889a50457e27f35a1a2d4a7dbc38a4994
Kevin Lutzer <klutzer@vendasta.com>
2020-02-26 21:14:43 -0600

    refactor so this works for every machine

--- client/internal/google-iot/config.go
+++ client/internal/google-iot/config.go
@@ -1,16 +1,39 @@
 package googleiot
 
-import "io/ioutil"
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+)
 
 const (
-	certPath   = "../certs"
-	deviceID   = "prototyping-device"
+	certPath   = "certs"
 	host       = "mqtt.googleapis.com"
 	port       = "8883"
 	projectID  = "iot-klutzer"
 	registryID = "devices-klutzer"
 	region     = "us-central1"
 )
+
+var (
+	deviceID string
+)
+
+func init() {
+	cmd := exec.Command("hostname")
+
+	output, err := cmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	deviceID = strings.ReplaceAll(string(output), "\n", "")
+	if deviceID == "" {
+		panic("DEVICE_ID environment is required")
+	}
+	fmt.Printf("Device ID used '%s'", deviceID)
+}
 
 // SSLCerts stores the strings for the certs needed for SSL/TLS
 type SSLCerts struct {
