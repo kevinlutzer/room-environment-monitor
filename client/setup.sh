6aea58f66292d87f0ffa25b1787fb0f19fa934e4
Kevin Lutzer <kevinlutzer@proton.me>
2024-07-21 18:08:17 -0400

    remove legacy server code, update ibom, etc

---
+++ client/setup.sh
@@ -0,0 +1,5 @@
+arduino-cli lib install PubSubClient
+arduino-cli lib install PM1006K
+arduino-cli lib install "Adafruit BME280 Library"
+arduino-cli lib install ArduinoJson
+arduino-cli lib install UUID
--- server/functions/src/api/index.ts
+++
@@ -1,3 +0,0 @@
-export { TelemetryPubsubHandler } from './telemetry';
-export { TelemetryEventList } from './telemetry-event';
-export { DeviceList } from './device';
