c03f3f4fdaa85371d80667ecc58037d81dc01ac6
Kevin Lutzer <kevinlutzer@proton.me>
2024-08-09 21:35:40 -0400

    add setup.sh, pcb and programmer files

--- client/setup.sh
+++ client/setup.sh
@@ -3,3 +3,4 @@ arduino-cli lib install PM1006K
 arduino-cli lib install "Adafruit BME280 Library"
 arduino-cli lib install ArduinoJson
 arduino-cli lib install UUID
+arduino-cli lib install "Adafruit NeoPixel"
